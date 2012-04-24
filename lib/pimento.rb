2544631a98ab007edb8c6ce1bf1419f671d5a934
youpy <youpy@buycheapviagraonlinenow.com>
2012-04-24 21:55:47 +0900

    use bundler

--- lib/pimento.rb
+++ lib/pimento.rb
@@ -1,4 +1,5 @@
 module Pimento
+  VERSION = '0.0.2'
 end
 
 require 'pimento/renderable'
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,10 +1,13 @@
-$LOAD_PATH.unshift(File.dirname(__FILE__))
 $LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '..', 'lib'))
-require 'pimento'
-require 'spec'
-require 'spec/autorun'
 
-Spec::Runner.configure do |config|
+require 'rubygems'
+require 'pimento'
+
+RSpec.configure do |config|
+  config.treat_symbols_as_metadata_keys_with_true_values = true
+  config.run_all_when_everything_filtered = true
+  config.filter_run :focus
+
   def fixture(filename)
     File.dirname(__FILE__) + '/fixtures/' + filename
   end
